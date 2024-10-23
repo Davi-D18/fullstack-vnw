ece0009a71651b5370a03dd401246308588301a3
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-10-23 11:08:10 -0300

    JS
    Aula sobre Tipos de dados e Estrutura Condicional

---
+++ -introducao-js/basico-js/2_variaveis.js
@@ -0,0 +1,15 @@
+// Váriaveis e condições
+
+let nomeDeUsuario = "Fredinho 90"; //string
+
+const numero = 50;
+
+const estamosEm2024 = true;
+
+// Estrutura Condicional
+
+if (numero === 50) {
+  console.log("O número é exatamente 50");
+} else {
+  console.log("Número não é 50");
+}
